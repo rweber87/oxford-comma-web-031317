389cff1537d7453c268793e73ffae411a3791466
Robert Weber <rob.weber87@gmail.com>
2017-02-09 18:38:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
 
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length == 3
+    return array.slice(0..1).join(", ") + ", and #{array[array.length - 1]}"
+  elsif array.length > 3
+    return array.slice(0..array.length - 2).join(", ") + ", and #{array[array.length - 1]}"
+  end
+
 end
